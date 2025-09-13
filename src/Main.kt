de6801abe9af92fb79430aa7eed6db6a869fe45e
TishkaPlay <tix.tot@yandex.ru>
2025-09-13 13:07:25 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,105 @@
+//const val PI = 3.1415
+fun main() {
+//    println("Hello wolrd!")
+//    print("Hello\n")
+//    println("Kotlin")
+//    println("powiqjefpoqiwjefpoiqjwehgpfoiqhpwoeijhqpwoei")
+//    val city: String = "Volgograd"
+//    println(city)
+//    var number: Int = 2
+//    number = 3
+//    Задание:
+//    val name: String = "Райнер"
+//    val yearOfBirth: Int = 1999
+//    var age: Int = 26
+//    age+= 10
+//    println(age)
+//    Задание еще одно:
+//    val a: Int = 1
+//    val b: Long = 1
+//    val c: Double = 1.0
+//    val d: Float = 1.0f
+//    val e: Char = 'c'
+//    val f: String = "сон со сладкими пирожными"
+//    val g: Any = 1
+//    println(a)
+//    println(b)
+//    println(c)
+//    println(d)
+//    println(e)
+//    println(f)
+//    println(g)
+    /*    val game = "Dota2"
+        val year = 2005
+        val message = "$game - пользовательская карта для Warcraft III, разрабатываемая с $year года"
+        println(message)*/
+    /*    val name = "Denis" // Ну в этой строчке мы объявляем переменную нейм и вставляем в нее дениса
+        val age = 18 // в этой объявляем переменную аге и присваиваем ей значение 18
+        println("Hello") // в этой строчке выводим в консоль хелло и переносим строчку
+        println("My name is $name") // в этой строчке выводим май нейм ис и вставляем значение переменной таме, а после переносим строчку
+        println("My age is $age") // в этой мы повторяем то же самое, но только с переменной аге
+        val a = 10 // объявляем переменную а, присваиваем ей значение 10
+        val b = 15 // объявляем переменную б, присваиваем ей значение 15
+        val c = a+b+8 // объявляем переменную ц, присваиваем ей значение выражения а+б+8
+        val str = c.toString() // объявляем переменную стр, присваиваем ей значение переменной ц, преобразованное в строку
+        println(str) // выводим в консоль переменную стр, а после переносим строчку
+        val numList = arrayOf(1,2,3) // видимо, создаем список в котором на нулевой позиции стоит значение 1, на первой  значение 2 и на второй значение 3
+        var x = 0 // обявляем переменную икс и присваиваем ей значение 0
+        while (x<3) { // запускаем цикл с условием, он будет выполняться, если икс меньше трех
+            println("Item $x is ${numList[x]}") // внутри цикла выводим сообщение с индексом и значением массива, которое индексу соответсвует
+            x = x+1 // прибавляем к переменной икс единицу
+        }*/ // закрываем цикл
+    // Задание
+    /*var a: Int = 7
+    var b: Double = 3.5
+    b.toInt()
+    println(a+b)
+    println(a/b)*/
+    // Задание
+    /*    var x: Double = 65.2
+        var isPunk = true
+        var message = "Hello"
+        isPunk = false
+        var y = "15"
+        y = y + "Hello"*/
+    // Задание
+    /*    print("Введите имя: ")
+        var name = readln()
+        print("Введите возраст: ")
+        var age = readln().toInt()
+        println("Через 5 лет вам будет ${age+5} лет")*/
+    // Задание
+    /*print("Введите число а: ")
+    var a = readln().toInt()
+    print("Введите число b: ")
+    var b = readln().toInt()
+    println("Сумма: ${a + b}")
+    println("Разность: ${a - b}")
+    println("Произведение: ${a * b}")
+    println("Частное: ${a / b}")
+    println("Остаток: ${a % b}")*/
+    // Задание
+    /*val totalSeconds = readln().toInt()
+    val hours = totalSeconds / 3600
+    val minutes = totalSeconds % 3600 / 60
+    val seconds = totalSeconds % 3600 % 60
+    println("$hours ч $minutes мин $seconds сек")*/
+    // Самостоятельные задание
+    // Задание 1
+    /*val favoriteGame = "starcraft2"
+    val favoriteDigit = 13
+    val PI = 3.1415
+    val favoriteChar = 'ж'
+    println(favoriteGame)
+    println(favoriteDigit)
+    println(PI)
+    println(favoriteChar)*/
+    // Задание 2
+    //vak a: Sting = "false //было
+    //val a: Int = 7 //стало
+    // Задание 3
+    val pi: Double = 3.14
+    val radius: Int = 5
+    println({ pi * radius * radius })
+}
+
